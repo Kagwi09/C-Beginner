ca6a4ccb2c3d016a39db873fd25047daba855d56
Kagwi09 <kagwimwangi.m@gmail.com>
2024-03-29 10:31:55 +0300

    multiple if statements ex

--- Nested if, else elif statements.py
+++ Nested if, else elif statements.py
@@ -2,17 +2,30 @@
 #Only people over the age of 8 and over 4.5 ft can ride the carousel
 #Adults are required to pay $ 10 while minors between 10 & 13 are required to pay $ 5
 #Minors between 13 & 17 pay $ 8
+#If a customer wants to take a photo, they will be charged an extra $ 2
 height = float(input('What is your height? (feet): '))
 if height >= 4.5:
     age = int(input('How old are you? : '))
-    if age >= 18:
-        print('Please pay $ 10')
-    elif 10 <= age <= 13:
-        print('Please pay $ 5')
-    elif 13 < age < 18:
-        print('Please pay $ 8')
+    if age >= 10:
+        bill = 0
+        str1 = 'Your ticket is $'
+        if 10 <= age <= 13:
+            bill = 5
+            print(f'{str1} 5')
+        elif 14 <= age <= 17:
+            bill = 8
+            print(f'{str1} 8')
+        else:
+            bill = 10
+            print(f'{str1} 10')
+
+        want_photo = input('Would you like to take a photo? (Y/N) : ')
+        if want_photo == 'Y' or want_photo == 'y':
+            bill = bill + 2
+            print(f'Your total bill is {bill}')
+
     else:
-        print("You are too young for this ride")
+        print('You are too young for this ride')
 else:
     print('You are too short for this ride')
 print('Thank you!')
